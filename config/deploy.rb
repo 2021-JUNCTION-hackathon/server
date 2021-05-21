90ce83c70aff808c5e2c2ff8bf05867309849f29
kbs4674 <kbs4674@naver.com>
2021-05-21 16:51:16 +0000

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,14 +56,16 @@ set :keep_releases, 0
 # end
 
 namespace :deploy do
-  before :linked_files, :set_file do
-    on roles(:app), in: :sequence, wait: 10 do
-      unless test("[ -f #{shared_path}/config/application.yml ]")
-        upload! 'config/application.yml', "#{shared_path}/config/application.yml"
-      end
-      
-      unless test("[ -f #{shared_path}/config/database.yml ]")
-        upload! 'config/database.yml', "#{shared_path}/config/database.yml"
+  namespace :check do
+    before :linked_files, :set_file do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/application.yml ]")
+          upload! 'config/application.yml', "#{shared_path}/config/application.yml"
+        end
+        
+        unless test("[ -f #{shared_path}/config/database.yml ]")
+          upload! 'config/database.yml', "#{shared_path}/config/database.yml"
+        end
       end
     end
   end
