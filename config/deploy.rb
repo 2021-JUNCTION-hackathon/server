c1ff43fcf45b8337613d68aa72b7031f497d7570
kbs4674 <kbs4674@naver.com>
2021-05-21 17:12:22 +0000

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,8 +53,8 @@ namespace :deploy do
           upload! 'config/database.yml', "#{shared_path}/config/database.yml"
         end
         
-        unless test("[ -f #{shared_path}/config/database.yml ]")
-          upload! 'config/server_init/init_rbenv.sh', "#{shared_path}/config/init_rbenv.sh"
+        unless test("[ -f #{shared_path}/config/server_init/init_rbenv.sh ]")
+          upload! 'config/server_init/init_rbenv.sh', "#{shared_path}/config/server_init/init_rbenv.sh"
         end
         
         execute "rbenv -v"
