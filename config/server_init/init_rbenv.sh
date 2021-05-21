e99e40a5a6bde06f83879fe79943b6ada466ed87
kbs4674 <kbs4674@naver.com>
2021-05-21 21:55:16 +0000

    .

--- config/server_init/init_rbenv.sh
+++ config/server_init/init_rbenv.sh
@@ -6,5 +6,4 @@ sudo apt-get update
 sudo apt-get install -y gcc make libssl-dev libreadline-dev zlib1g-dev
 sudo apt-get update
 wget -q https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
-exec bash -l
-rbenv install 3.0.1
+exec bash -l > rbenv install 3.0.1
