1254647182998f6f0e45125300c0c4352d27f7a9
kbs4674 <kbs4674@naver.com>
2021-05-21 18:33:16 +0000

    .

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :user, 'kbs4674'
 ## [Example] set :user, 'ubuntu'
 
 ## REMOTE 서버 설정
-set :remote_server_ip, '20.194.29.163'
+set :remote_server_ip, '52.231.48.54'
 
 ## Remote서버에 대해 배포환경 설정 (환경변수 설정)
 set :rails_env, 'production'
--- config/server_init/init_rbenv.sh
+++ config/server_init/init_rbenv.sh
@@ -5,5 +5,5 @@ source ~/.bashrc
 sudo apt-get update
 sudo apt-get install -y gcc make libssl-dev libreadline-dev zlib1g-dev
 sudo apt-get update
-wget -q https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
+wget -qS https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
 rbenv install 3.0.1
