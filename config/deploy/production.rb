ca02fd13f7d63e4b177bc45f422662c434fd5d0c
kbs4674 <kbs4674@naver.com>
2021-05-21 17:00:57 +0000

    FIX

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,45 +19,26 @@ server fetch(:remote_server_ip), port: 22, roles: [:web, :app, :db], primary: tr
 ## SSH Remote 설정 (서버 아이디 및 pem Key 경로 설정)
 set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w[/home/ubuntu/environment/azure_key.pem] }
 
+
 before :deploy, "deploy:rbenv_init"
-before :deploy, "deploy:test_init"
-# rbenv 설치
-# set :rbenv_ruby, '3.0.1'
 namespace :deploy do
+  # rbenv 설치
   task :rbenv_init do
     on roles(:app) do
-      execute 'echo "good"'
       execute "rbenv -v"
     rescue StandardError => e
-      execute 'echo "bad"'
-      # execute "cd #{fetch(:application)}/public/server_init/int_rbenv"
+      execute "cd #{fetch(:application)}/public/server_init/int_rbenv"
     end
   end
   
-  task :test_init do
-    on roles(:app) do
-      execute 'echo "yo! good"'
-      execute "rbenv -v"
-    rescue StandardError => e
-      execute 'echo "h.... bad"'
-      # execute "cd #{fetch(:application)}/public/server_init/int_rbenv"
-    end
-  end
-end
-
-## swagger 파일 업로드
-# cap production deploy:swagger_init
-namespace :deploy do
+  # swagger 파일 업로드
   task :swagger_init do
     on roles(:app) do
       execute "cd ~/#{fetch(:application)}/current/public; mv swagger.tar.gz swagger; cd swagger; tar -xvf swagger.tar.gz"
     end
   end
-end
-
-## [Rails Version 5.2 ~] config/application.yml 파일을 EC2 서버로 Upload
-# cap production deploy:env_file_upload
-namespace :deploy do
+  
+  # config/application.yml 파일을 EC2 서버로 Upload
   task :env_file_upload do
     on roles(:app) do
       upload! 'config/application.yml', "#{shared_path}/config/application.yml"
