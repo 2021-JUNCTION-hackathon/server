8e296581dd1a68d1e6cbb92b4cd8155c4cac2328
kbs4674 <kbs4674@naver.com>
2021-05-21 17:34:10 +0000

    .

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :user, 'kbs4674'
 ## [Example] set :user, 'ubuntu'
 
 ## REMOTE 서버 설정
-set :remote_server_ip, '20.194.61.63'
+set :remote_server_ip, '52.231.66.165'
 
 ## Remote서버에 대해 배포환경 설정 (환경변수 설정)
 set :rails_env, 'production'
--- config/server_init/init_rbenv.sh
+++ config/server_init/init_rbenv.sh
@@ -3,11 +3,8 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 sudo apt-get update
-sudo apt-get install -y gcc
+sudo apt-get install -y gcc make libssl-dev libreadline-dev zlib1g-dev
 sudo apt-get update
-sudo apt-get install -y make
-sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
-rbenv versions
 curl -fsSL curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash | bash
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-doctor | bash
 rbenv install 3.0.1
