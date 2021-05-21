4c59c9ca9d08a13b16607f4298af4488b4f47ad4
kbs4674 <kbs4674@naver.com>
2021-05-21 17:49:19 +0000

    .

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :user, 'kbs4674'
 ## [Example] set :user, 'ubuntu'
 
 ## REMOTE 서버 설정
-set :remote_server_ip, '52.231.10.73'
+set :remote_server_ip, '20.194.16.89'
 
 ## Remote서버에 대해 배포환경 설정 (환경변수 설정)
 set :rails_env, 'production'
@@ -18,7 +18,6 @@ server fetch(:remote_server_ip), port: 22, roles: [:web, :app, :db], primary: tr
 
 ## SSH Remote 설정 (서버 아이디 및 pem Key 경로 설정)
 set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w[/home/ubuntu/environment/azure_key.pem] }
-
 
 
 namespace :deploy do
