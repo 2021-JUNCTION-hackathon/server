b11dd401e96b4d8f40c685949b7360a1db8f889d
kbs4674 <kbs4674@naver.com>
2021-05-21 17:18:44 +0000

    .

--- config/server_init/init_rbenv.sh
+++ config/server_init/init_rbenv.sh
@@ -2,6 +2,12 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
+sudo apt-get update
+sudo apt-get install -y gcc
+sudo apt-get update
+sudo apt-get install -y make
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+rbenv versions
 curl -fsSL curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash | bash
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-doctor | bash
 rbenv install 3.0.1
