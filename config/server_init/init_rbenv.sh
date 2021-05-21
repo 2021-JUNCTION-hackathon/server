5e64a50f1558c95373fef84fa5203f785cd27911
kbs4674 <kbs4674@naver.com>
2021-05-21 19:49:18 +0000

    .

--- config/server_init/init_rbenv.sh
+++ config/server_init/init_rbenv.sh
@@ -1,3 +1,4 @@
+echo '..............≥>>>>>>>>'
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
