e4fe08e98016bbc7ffca26a27f9e1ad48fde16c9
kbs4674 <kbs4674@naver.com>
2021-05-22 04:50:02 +0000

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,7 +58,7 @@ namespace :deploy do
         end
         
         execute 'ready..'
-        execute "ls ~/#{fetch(:application)}/shared/config/server_init"
+        execute "ls ./#{fetch(:application)}/shared/config/server_init"
         execute "cat ~/#{fetch(:application)}/shared/config/server_init/work.txt"
         execute 'good..'
       rescue StandardError => e
