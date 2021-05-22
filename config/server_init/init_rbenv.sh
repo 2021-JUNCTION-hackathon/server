366970ec68d95009f43ef55ad4b2b45f45faf771
kbs4674 <kbs4674@naver.com>
2021-05-22 04:06:53 +0000

    .

--- config/server_init/init_rbenv.sh
+++ config/server_init/init_rbenv.sh
@@ -1,17 +1,12 @@
-# git clone git://github.com/sstephenson/rbenv.git .rbenv
-# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-# echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-# source ~/.bashrc
-# sudo apt-get update
-# sudo apt-get install -y gcc make libssl-dev libreadline-dev zlib1g-dev
-# sudo apt-get update
-# wget -q https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
-# rbenv install 3.0.1
-
-cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get update
+sudo apt-get install -y gcc make libssl-dev libreadline-dev zlib1g-dev
+sudo apt-get update
+wget -q https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+rbenv install 3.0.1
