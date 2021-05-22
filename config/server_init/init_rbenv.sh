365c76065c104106967a19c3b3b77af57d1fa5ec
kbs4674 <kbs4674@naver.com>
2021-05-22 04:37:09 +0000

    .

--- config/server_init/init_rbenv.sh
+++ config/server_init/init_rbenv.sh
@@ -1,12 +1,17 @@
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+# git clone git://github.com/sstephenson/rbenv.git .rbenv
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+# echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+# source ~/.bashrc
+# sudo apt-get update
+# sudo apt-get install -y gcc make libssl-dev libreadline-dev zlib1g-dev
+# sudo apt-get update
+# wget -q https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+# echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+# source ~/.bash_profile
+# rbenv install 3.0.1
+rbenv global 3.0.1
+
 sudo apt-get update
-sudo apt-get install -y gcc make libssl-dev libreadline-dev zlib1g-dev
-sudo apt-get update
-wget -q https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
-rbenv install 3.0.1
+sudo apt-get -y install libpq-dev postgresql postgresql-contrib
+
